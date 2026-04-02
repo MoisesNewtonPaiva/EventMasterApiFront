08a574f68a088ab79a4a955de7239651527c7a3b
MoisesNewtonPaiva <202351050786@alunos.newtonaiva.edu.br>
2026-04-02 14:06:06 -0300

    feat: create PageId component to fetch and display event details by ID

--- src/PageId.jsx
+++ src/PageId.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
+import './PageId.css';
 
 function PageId() {
     const { id } = useParams();
